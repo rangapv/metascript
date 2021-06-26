4c6b13458afe3c470e371ada7e0f221bb2c49ec4
rangapv <rangapv@yahoo.com>
2021-06-26 06:35:57 +0000

    Fixed a typo BUG

--- kubemaster.sh
+++ kubemaster.sh
@@ -3,6 +3,6 @@ set -E
 #Install the runtime in this case it is the containerd
 source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/runcontainerD.sh)
 #Install the k8s components and customize the runtime to use systemd
-spurce <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/adm_containerd_install.sh)
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/adm_containerd_install.sh)
 #Call the init.d command
 source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sinit.sh)
