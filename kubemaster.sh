98f0159f292cb6f05a346d3b9eff981d615d190e
rangapv <rangapv@yahoo.com>
2021-06-27 06:43:07 +0000

    Added flannel and Dashboard support

--- kubemaster.sh
+++ kubemaster.sh
@@ -6,3 +6,5 @@ source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/runcont
 source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/adm_containerd_install.sh)
 #Call the init.d command
 source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sinit.sh)
+#Call the flannel and dashboard install
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sfladash.sh)
