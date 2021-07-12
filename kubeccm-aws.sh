445b9305a4661f80c69982497b9a246610a029fd
rangapv <rangapv@yahoo.com>
2021-07-12 13:22:36 +0000

    Modifed instruction for kubeccm aws script

--- kubeccm-aws.sh
+++ kubeccm-aws.sh
@@ -2,8 +2,10 @@
 set -E
 #Create the /etc/kubernetes/cloud.conf
 #Create the secrets file to be referenced by the cloud controllerd contaienr in this ccm-aws.yaml file
-#sc1=`kubectl create secret generic aws-secret --from-file=cloud.conf=/etc/kubernetes/cloud.conf --from-file=config=$HOME/.kube/config --from-file=AWS_ACCESS_KEY_ID=$HOME/access_id --from-file=AWS_SECRET_ACCESS_KEY=$HOME/secret_id -n kube-system`
-
+#sc1=`kubectl create secret generic aws-secret --from-file=cloud.conf=/etc/kubernetes/cloud.conf --from-file=config=$HOME/.kube/config -n kube-system`
+#create the foloowing two secrets with the right access and secret"
+#t1=`kubectl create secret generic aws-access-1 --from-literal=AWS_ACCESS_KEY_ID=xxxxxxxx -n kube-system`
+#t2=`kubectl create secret generic aws-access-2 --from-literal=AWS_SECRET_ACCESS_KEY=xxxxxxxx -n kube-system`
 #Install the CCM RBAC 
 sc2=`kubectl apply -f  https://raw.githubusercontent.com/rangapv/k8s/master/ccm/ccm-rbac.yaml`
 echo "ccm RBAC output is $sc2"
