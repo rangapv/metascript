826b104efed372a93c02f7924003f0cb42034853
rangapv <rangapv@yahoo.com>
2021-07-15 14:36:53 +0000

    This is the First script that needs to be run with arguments acces_id secret_id before k8s install

---
+++ metasetup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#Run this script like: ./metasetup.sh access_id secret_id
+set -E
+source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh)
+
+cont() {
+acid="$1"
+secid="$2"
+export AWS_ACCESS_KEY_ID=${acid}
+export AWS_SECRET_ACCESS_KEY=${secid}
+sudo $cm1 -y install awscli
+sudo $cm1 -y install jq
+}
+
+
+hid() {
+wget http://s3.amazonaws.com/ec2metadata/ec2-metadata >/dev/null 2>&1
+chmod u+x ec2-metadata
+str23=`./ec2-metadata -i`
+str231=`./ec2-metadata -z | awk '{split($0,a," "); print a[2]}' | sed 's/.$//'`
+str232=`echo $str23 | awk '{split($0,a," "); print a[2]}'`
+#echo "region $str231"
+#echo "id $str232"
+
+}
+
+hpdns() {
+pdns=`aws ec2 describe-instances --region=${str231} --instance-ids ${str232} | jq -r '.Reservations[].Instances[].PrivateDnsName'`
+#echo "pdns $pdns"
+}
+
+
+hcfg() {
+file1="/etc/cloud/cloud.cfg"
+str2="preserve_hostname: true"
+str1="preserve_hostname:"
+sudo sed -i "s|${str1}.*|${str2}|" $file1
+}
+
+
+hcf() {
+
+file2="/etc/hosts"
+str3="127.0.0.1 localhost"
+str4="127.0.0.1 localhost ${pdns}"
+sudo sed -i "s|${str3}.*|${str4}|" $file2
+sudo hostnamectl set-hostname ${pdns}
+}
+
+
+cont $1 $2
+hid
+hpdns
+
+if [ -z "$mac" ]
+then
+
+if [ ! -z "$u1" ]
+then
+   if [ $ki = "ubuntu" ]
+   then
+     hcfg
+     hcf
+   fi
+fi #end of u1 check
+
+fi #end of mac check
