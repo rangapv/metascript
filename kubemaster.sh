dbf1ef79e1005b56bde444ee39af57745f9a8f10
rangapv <rangapv@yahoo.com>
2021-07-16 08:23:19 +0000

    Added the setup script to be sourced in master and nodes for hostname private DNS and cloud.conf

--- kubemaster.sh
+++ kubemaster.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 set -E
+#Prepare the node in aws so that k8s can get hostanme as private DNS name
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metasetup.sh $1 $2)
 #Install the runtime in this case it is the containerd
 source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/runcontainerD.sh)
 #Install the k8s components and customize the runtime to use systemd
@@ -8,3 +10,5 @@ source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/a
 source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sinit.sh)
 #Call the flannel and dashboard install
 source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sfladash.sh)
+#Call the create cloud.conf and secrets file on the master node cluster
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metamaster.sh)
--- kubenode.sh
+++ kubenode.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
 set -E
+#Prepare the node in aws so that k8s can get hostanme as private DNS name
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metasetup.sh $1 $2)
 #Install the runtime in this case it is the containerd
 source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/runcontainerD.sh)
 #Install the k8s components and customize the runtime to use systemd
 source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_node/k8snodeinstall.sh)
+#Install the flannel yaml, call this after transfering config file from master hence commented
 #source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sfladash.sh)
+#Install the cloud.conf for cloud-controller loops
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metanode.sh)
