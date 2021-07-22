d27a69954f23bfa9e5bf02b68042d8b0808b35e1
rangapv <rangapv@yahoo.com>
2021-07-22 09:04:12 +0000

    Added CNI specific branches

---
+++ AWS/flannel/kubemaster.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+set -E
+#Prepare the node in aws so that k8s can get hostanme as private DNS name
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metasetup.sh $1 $2)
+#Install the runtime in this case it is the containerd
+source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/runcontainerD.sh)
+#Call the init.d command
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sinit.sh)
+#Call the create cloud.conf and secrets file on the master node cluster
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metamaster.sh)
+#Call the flannel and dashboard install
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sfladash.sh)
+#Call the Cloud Controller Manager for AWS by the following script if you Dont need this but only vanilla cluster comment-it-out
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/kubeccm-aws.sh)
---
+++ AWS/flannel/kubenode.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+set -E
+#Prepare the node in aws so that k8s can get hostanme as private DNS name
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metasetup.sh $1 $2)
+#Install the runtime in this case it is the containerd
+source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/runcontainerD.sh)
+#Install the k8s components and customize the runtime to use systemd
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_node/k8snodeinstall.sh)
+#Install the cloud.conf for cloud-controller loops,call this after transfering config file from master hence commented
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metanode.sh)
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/nodeconfig.sh)
+#For Cloud-Controller-Manager modify the kubelet...on the nodes...
+#Execute kubectl join by copying the line from Master node got during init.... then..install flannel below
+#Install the flannel yaml
+#source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_node/k8snodeflanel.sh)
