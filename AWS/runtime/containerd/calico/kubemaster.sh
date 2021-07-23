115be7b0d14b569e11bf472c2e4647384093f153
rangapv <rangapv@yahoo.com>
2021-07-23 07:17:06 +0000

    AWS specific claico file

--- AWS/runtime/containerd/calico/kubemaster.sh
+++ AWS/runtime/containerd/calico/kubemaster.sh
@@ -3,6 +3,6 @@ set -E
 #Install the runtime in this case it is the containerd
 source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/runcontainerD.sh)
 #Call the init.d command
-source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sinit-adm.sh)
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sinit-adm-calico-aws.sh)
 #Call the flannel and dashboard install
 source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8scalicodash.sh)
