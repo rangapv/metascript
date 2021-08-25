7e902314f73de8b80f22c41af18b735ae281e4e8
rangapv <rangapv@yahoo.com>
2021-08-25 12:27:08 +0000

    Added init script for cloud to install python and runtime as FIRST step

---
+++ metainit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -E
+#Install the python version
+source <(curl -s https://raw.githubusercontent.com/rangapv/ansible-install/main/py.sh)
+#Install the doker
+source <(curl -s https://raw.githubusercontent.com/rangapv/doker/master/dock.sh)
