15ce6e3518ac5d3e53ee8b4386a465f409aaacc6
rangapv <rangapv@yahoo.com>
2021-09-11 07:42:03 +0000

    Added node install instruction steps

--- cloud-agnostic/cni/flannel/runtime/dockerd/kubenode.sh
+++ cloud-agnostic/cni/flannel/runtime/dockerd/kubenode.sh
@@ -4,8 +4,9 @@ set -E
 source <(curl -s https://raw.githubusercontent.com/rangapv/doker/master/dock.sh)
 #Install the k8s components and customize the runtime to use systemd
 source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_node/k8snodeinstall.sh)
+#Execute the following line after copying the config file from the master via ftp
 source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/nodeconfig.sh)
-#Execute kubectl join by copying the line from Master node got during init.... then..install flannel below
+#Execute kubectl join by copying the line from Master node got during init(It is in the flag.txt).... then..install flannel below
 #Install the flannel yaml
 #source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sfladash.sh)
 #      or
