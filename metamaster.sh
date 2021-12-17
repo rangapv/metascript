2bc036aa42f8bc165daafd7d0b15c3e5dabd57eb
rangapv <rangapv@yahoo.com>
2021-12-17 13:46:40 +0000

    Changed the secret name to match the ccm container manifest

--- metamaster.sh
+++ metamaster.sh
@@ -9,7 +9,7 @@ ac2="echo $AWS_SECRET_ACCESS_KEY"
 
 sec1=`kubectl create secret generic aws-access-1 --from-literal=AWS_ACCESS_KEY_ID=${acid} -n kube-system`
 sec2=`kubectl create secret generic aws-access-2 --from-literal=AWS_SECRET_ACCESS_KEY=${secid} -n kube-system`
-sec3=`kubectl create secret generic aws-secret --from-file=cloud.conf=/etc/kubernetes/cloud.conf --from-file=config=$HOME/.kube/config -n kube-system`
+sec3=`kubectl create secret generic aws-ccm-secret --from-file=cloud.conf=/etc/kubernetes/cloud.conf --from-file=config=$HOME/.kube/config -n kube-system`
 
 }
 
