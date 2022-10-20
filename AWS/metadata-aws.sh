a15275ed453364ce128e9d48c4ab96a2dc30252b
rangapv <rangapv@yahoo.com>
2022-10-20 12:58:36 +0000

    Added meta data extract like instance-id, region for AWS current instance

---
+++ AWS/metadata-aws.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -E
+
+hid() {
+wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
+chmod u+x ec2-metadata
+str23=`./ec2-metadata -i`
+str233=`./ec2-metadata -z | awk '{split($0,a," "); print a[2]}' | sed 's/.$//'`
+str231=`./ec2-metadata -z | awk '{split($0,a," "); print a[2]}'`
+str232=`echo $str23 | awk '{split($0,a," "); print a[2]}'`
+#echo "region $str231"
+#echo "id $str232"
+
+}
+
+hid
